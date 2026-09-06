d8fb38273cd55e659247ad8c05796504cfeadada
hanoverp4009 <hanoverp4009@student.faytechcc.edu>
2026-09-06 14:55:44 -0400

    First Python Assignment 1 P1LAB1
    getting input from the user and displaying output.

---
+++ Paul Hanover P1LAB1.py
@@ -0,0 +1,11 @@
+#Paul Hanover
+
+# Date:  6 September 2026
+
+# P1LAB1
+
+# This assignment teaches the student how to create a python program using Visual Studio Code. The goal is to get input from the user and display output.
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello, ", first_name, last_name, "!", "Welcome to CTI-110")
